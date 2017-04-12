f3e7b3c45e573f3645a7eef9a7880bea134082b4
Himanshu <himanshu.raj@spotdy.com>
2017-04-12 18:41:01 +0530

    stop word  filtering

---
+++ stopWord.py
@@ -0,0 +1,25 @@
+from nltk.corpus import stopwords
+
+x = stopwords.words('english')
+
+#print x
+
+from nltk.tokenize import word_tokenize
+
+example_sent = "This is a sample sentence, showing off the stop words filtration."
+
+stop_words = set(stopwords.words('english'))
+
+word_tokens = word_tokenize(example_sent)
+
+filtered_sentence = [w for w in word_tokens if not w in stop_words] # this is similiar to down loop
+
+filtered_sentence = []
+
+for w in word_tokens:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+
+print(word_tokens)
+print(filtered_sentence)
+
